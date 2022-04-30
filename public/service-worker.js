06353b795d6cf7313e51cb1a8000c245d0ccd743
kstensing <stensingk@gmail.com>
2022-04-29 22:13:44 -0500

    initial structure to service-worker file

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js"
+]
+
+self.addEventListener('install', function (e) {
+
+})
